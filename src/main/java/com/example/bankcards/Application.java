5e8175e13b1c3634b07fa7f0a7f5ba5f2a8590a5
Armen23023 <mirzoyana805@gmail.com>
2025-09-30 10:16:45 +0400

    add docker-compose.yml for Spring Boot app and MySQL services, and initialize default admin via CommandLineRunner

--- src/main/java/com/example/bankcards/Application.java
+++ src/main/java/com/example/bankcards/Application.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @RequiredArgsConstructor
